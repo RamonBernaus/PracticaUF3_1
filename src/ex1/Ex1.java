8841c3c98f91b86dd1af94522b686fa81ae8587c
Ramon <Ramon@DESKTOP-LF6B84F>
2022-02-14 20:45:00 +0100

    Comentar

--- src/ex1/Ex1.java
+++ src/ex1/Ex1.java
@@ -1,7 +1,7 @@
 /*
 1. Crea un programa que llegeixi frases per teclat i les vagi guardant en un fitxer
 “textos.txt”. El programa finalitzarà quan s’entri una cadena buida
-*/
+ */
 package ex1;
 
 import java.io.BufferedReader;
@@ -19,50 +19,55 @@ import java.util.Scanner;
 public class Ex1 {
 
     public static void main(String[] args) throws IOException {
-
-        String linea;
+        //Iniciem el programa
         System.out.println("Aquest programa llegeix i guarde els fitxers tipo text que donis.");
         System.out.println("Així que...");
+        //Executem el File_read per a que ens llegeixi
         File_read();
-        
-        }
-    public static String File_read() throws IOException{
+
+    }
+
+    public static String File_read() throws IOException {
+        //Creem el file 
         File arxiu = new File("./arxiu.txt");
-        Scanner s = new Scanner(System.in);
         arxiu.createNewFile();
+        Scanner s = new Scanner(System.in);
         FileReader fr = new FileReader(arxiu);
+        //Fem que el llegeixi
         BufferedReader br = new BufferedReader(fr);
-        String linea = null;
-        boolean comprovar = true;
-        do{
-        try {
-        System.out.println("Digues una frase ");
-        linea = br.readLine();
-        linea= s.nextLine();       
-        if (linea.equals("")){
-            System.out.println("La linea esta buida, el programa s'ha aturat");
-            fr.close();
-            comprovar = false;
-            return linea;
-        } else{
-            comprovar = true;          
-            File_print(linea);
-        }
-        } catch (Exception e) {
-            e.printStackTrace();
-            
-        }
-        }while(comprovar = true);
-        return linea;
+        String linea = null; //Creem un string per a que es doni el contingut
+        boolean comprovar = true; //boolean per comprovar
+        do {
+            try {
+                System.out.println("Digues una frase ");
+                linea = br.readLine(); //imprimim el contingu al file
+                linea = s.nextLine(); //el llegim per a poguer saber si es null
+                if (linea.equals("")) { //comprovem si es null
+                    System.out.println("La linea esta buida, el programa s'ha aturat");
+                    fr.close();
+                    comprovar = false;
+                    return linea;
+                    //Si ho es para el programa
+                } else {
+                    comprovar = true;
+                    //Si no ho es segueix i ens ho registra en el arxiu
+                    File_print(linea); //amb el file print
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+        } while (comprovar = true);
+        return linea; //tornem el string al main
     }
 
     public static File File_print(String F) throws IOException {
-        File arxiu = new File("./arxiu.txt");
-        FileWriter write = new FileWriter(arxiu, true);
-        PrintWriter pw = new PrintWriter(write);
-        pw.println(F);
+        File arxiu = new File("./arxiu.txt"); //Cridem al file
+        FileWriter write = new FileWriter(arxiu, true); 
+        PrintWriter pw = new PrintWriter(write); //el copiem al arxiu
+        pw.println(F); 
         pw.flush();
-        write.close();
+        write.close(); //parem 
         return arxiu;
     }
 
