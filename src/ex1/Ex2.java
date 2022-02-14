87ace6f91b925bf314a5bf6249c4030ad0eab98d
Ramon <Ramon@DESKTOP-LF6B84F>
2022-02-14 20:20:10 +0100

    Ex1 i 2 arreglat

--- src/ex1/Ex2.java
+++ src/ex1/Ex2.java
@@ -3,40 +3,18 @@
 a) Si el fitxer no existeix, que el generi.
 b) Si el fitxer ja existeix, que afegeixi el contingut al final.
 c) Si l’usuari escriu @ESBORRA, cal esborrar el contingut del fitxer.
-*/
+ */
 package ex1;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Scanner;
 
 public class Ex2 {
 
     public static void main(String[] args) throws IOException {
-        File F = new File("./arxiu.txt");
-        F.createNewFile();
-        FileReader fr = new FileReader(F);
-        FileWriter writer = new FileWriter(F,true);
-        BufferedReader br = new BufferedReader(fr);
-        PrintWriter pw = new PrintWriter(writer);
-        Scanner s = new Scanner(System.in);
-        try {
-            String linea;
-            System.out.println("Digues una frase ");
-            linea = s.nextLine();
-            linea = br.readLine();
-            pw.println(linea);
-            pw.flush();
-            writer.close();
-            fr.close();           
-        } catch (Exception e) {
-            e.printStackTrace();
-            
+        File F = new File("./arxiu_1.txt");
+        if (F.exists()) {
+            F.createNewFile();
         }
-
     }
 }
