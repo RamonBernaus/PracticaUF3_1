1ac5f96dc74963112b81f1399a34e81301d97798
ausias <ausias@h7-42-THINK-2020-20>
2022-02-14 10:17:18 +0100

    penjar

---
+++ src/ex1/Ex2.java
@@ -0,0 +1,17 @@
+/*1. Crea un programa que llegeixi frases per teclat i les vagi guardant en un fitxer
+“textos.txt”. El programa finalitzarà quan s’entri una cadena buida*/
+package ex1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ex2 {
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
